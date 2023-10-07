54ae4cce655ac3b28d8bdb52e8e209e1af6dd715
sfilipovski <49661929+sfilipovski@users.noreply.github.com>
2023-10-07 23:38:43 +0200

    add state to Balance

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from '../context/GlobalState'
 
 export const Balance = () => {
+    const { transactions } = useContext(GlobalContext)
+
+    const amount = transactions.map(item => item.amount)
+    const balance = amount.reduce((total, item) => total+=item, 0).toFixed(2)
+    console.log(balance)
+
     return (
         <>
             <h4>Your Balance</h4>
-            <h1>$0.00</h1>
+            <h1>${balance}</h1>
         </>
     )
 }
--- src/components/IncomeExpenses.jsx
+++ src/components/IncomeExpenses.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const IncomeExpenses = () => {
+
+  const { transactions } = useContext(GlobalContext)
+
+  const amount = transactions.map(item => item.amount)
+  const income = amount.filter(item => item > 0).reduce((total, item) =>  item>=0?total+=item:total+=0, 0).toFixed(2)
+  const expense = amount.filter(item => item < 0).reduce((total, item) =>  item<0?total+=item:total+=0, 0)
+
   return (
     <div className="inc-exp-container">
         <div>
             <h4>Income</h4>
-            <p className='money plus'>+$0.00</p>
+            <p className='money plus'>+${income}</p>
         </div>
         <div>
             <h4>Expense</h4>
-            <p className='money minus'>-$0.00</p>
+            <p className='money minus'>-${Math.abs(expense).toFixed(2)}</p>
         </div>
     </div>
   )
