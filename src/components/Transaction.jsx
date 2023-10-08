1f8f81371a1c5bfc032cf278da23f0ebcc520832
sfilipovski <49661929+sfilipovski@users.noreply.github.com>
2023-10-08 02:10:00 +0200

    feat-add/delete-transaction

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 export const Transaction = ({ transaction }) => {
 
-    const sign = transaction.amount<0 ? '-' : '+'
+  const { deleteTransaction } = useContext(GlobalContext) 
+
+  const sign = transaction.amount<0 ? '-' : '+'
 
   return (
     <li className={transaction.amount<0 ? 'minus' : 'plus'}>
       {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
-      <button className="delete-btn">x</button>
+      <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
     </li>
   );
 }
